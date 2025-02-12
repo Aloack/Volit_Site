f847be4ba6bf65ee3681510d13cd03857f4895f3
gabriel-2470 <gabrielfernando224444@hotmail.com>
2025-02-12 02:28:07 -0300

    Feat: Criação Footer

--- aplicacaoweb/src/app/pages/home/home/home.component.ts
+++ aplicacaoweb/src/app/pages/home/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../homePages/header/header.component";
 import { RouterModule } from '@angular/router';
+import { FooterComponent } from "../homePages/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone:true,
-  imports: [HeaderComponent, RouterModule],
+  imports: [HeaderComponent, RouterModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
