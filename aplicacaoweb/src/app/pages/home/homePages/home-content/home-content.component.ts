b41ac6833baa6d172f6e42f4ef7ade6996a868f8
Gabriel Fontes <bielfontes6782@gmail.com>
2025-02-11 23:49:51 -0300

    Feat: padronizando rotas e componente de contato

--- aplicacaoweb/src/app/pages/home/homePages/home-content/home-content.component.ts
+++ aplicacaoweb/src/app/pages/home/homePages/home-content/home-content.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home-content',
-  standalone:true,
+  standalone: true,
   imports: [],
   templateUrl: './home-content.component.html',
   styleUrl: './home-content.component.scss'
