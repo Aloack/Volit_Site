6ae1ab697942a2b715c25e36e68fd3991350d03e
Gabriel Fontes <136857551+GabrielFontes3094@users.noreply.github.com>
2025-02-21 00:47:29 -0300

    Feat: apontamento botao header (#52)
    Co-authored-by: Gabriel Fontes <bielfontes6782@gmail.com>

--- aplicacaoweb/src/app/pages/home/homePages/header/header.component.ts
+++ aplicacaoweb/src/app/pages/home/homePages/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,12 +10,21 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent {
-
   menuAberto = false;
+
+  constructor(private router: Router) {}
 
   toggleMenu() {
     this.menuAberto = !this.menuAberto;
   }
 
+  scrollToSection(sectionId: string) {
+    this.router.navigate(['/home']).then(() => {
+      const element = document.getElementById(sectionId);
+      if (element) {
+        element.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
-
+}
